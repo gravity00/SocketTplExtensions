be5dcb16b5b80ca4543dc711894eb55d1ab7e28d
João Simões <nogravity00@outlook.com>
2019-06-24 18:58:36 +0100

    DisconnectAsync extensions

--- src/SocketTplExtensions/SocketExtensions.cs
+++ src/SocketTplExtensions/SocketExtensions.cs
@@ -71,11 +71,42 @@ namespace System.Net.Sockets
             try
             {
                 ((Socket) tcs.Task.AsyncState).EndConnect(ar);
-                tcs.SetResult(true);
+                tcs.TrySetResult(true);
             }
             catch (Exception e)
             {
-                tcs.SetException(e);
+                tcs.TrySetException(e);
+            }
+        };
+
+        #endregion
+
+        #region DisconnectAsync
+
+        /// <summary>
+        /// Begins an asynchronous request to disconnect from a remote endpoint.
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="reuseSocket"></param>
+        /// <returns></returns>
+        public static Task DisconnectAsync(this Socket socket, bool reuseSocket)
+        {
+            var tcs = new TaskCompletionSource<bool>(socket);
+            socket.BeginDisconnect(reuseSocket, BeginDisconnectCallback, tcs);
+            return tcs.Task;
+        }
+
+        private static readonly AsyncCallback BeginDisconnectCallback = ar =>
+        {
+            var tcs = (TaskCompletionSource<bool>) ar.AsyncState;
+            try
+            {
+                ((Socket) tcs.Task.AsyncState).EndDisconnect(ar);
+                tcs.TrySetResult(true);
+            }
+            catch (Exception e)
+            {
+                tcs.TrySetException(e);
             }
         };
 
