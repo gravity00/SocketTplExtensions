3e1a3eebcacaa2c8ae1376eec6e2f3243e343360
João Simões <nogravity00@outlook.com>
2019-06-24 22:26:02 +0100

    Language version

--- examples/SocketTplExtensions.ServerExample/Program.cs
+++ examples/SocketTplExtensions.ServerExample/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace SocketTplExtensions.ServerExample
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Application started...");
+
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Press <enter> to exit...");
+            Console.ReadLine();
         }
     }
 }
