a8b2543a486905ae79ad96f72e119a201bdc95bb
João Simões <nogravity00@outlook.com>
2019-06-24 19:27:39 +0100

    SendAsync extensions

--- src/SocketTplExtensions/SocketExtensions.cs
+++ src/SocketTplExtensions/SocketExtensions.cs
@@ -188,5 +188,52 @@ namespace System.Net.Sockets
         };
 
         #endregion
+
+        #region SendAsync
+
+        /// <summary>
+        /// Sends data asynchronously to a connected <see cref="Socket"/>.
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="size"></param>
+        /// <param name="socketFlags"></param>
+        /// <returns></returns>
+        public static Task<int> SendAsync(this Socket socket, byte[] buffer, int offset, int size, SocketFlags socketFlags)
+        {
+            var tcs = new TaskCompletionSource<int>(socket);
+            socket.BeginSend(buffer, offset, size, socketFlags, BeginSendCallback, tcs);
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Sends data asynchronously to a connected <see cref="Socket"/>.
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="buffers"></param>
+        /// <param name="socketFlags"></param>
+        /// <returns></returns>
+        public static Task<int> SendAsync(this Socket socket, IList<ArraySegment<byte>> buffers, SocketFlags socketFlags)
+        {
+            var tcs = new TaskCompletionSource<int>(socket);
+            socket.BeginSend(buffers, socketFlags, BeginSendCallback, tcs);
+            return tcs.Task;
+        }
+
+        private static readonly AsyncCallback BeginSendCallback = ar =>
+        {
+            var tcs = (TaskCompletionSource<int>) ar.AsyncState;
+            try
+            {
+                tcs.TrySetResult(((Socket) tcs.Task.AsyncState).EndSend(ar));
+            }
+            catch (Exception e)
+            {
+                tcs.TrySetException(e);
+            }
+        };
+
+        #endregion
     }
 }
