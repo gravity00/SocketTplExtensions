a8362faff9cafa8fa30723ec95653796704b594a
João Simões <nogravity00@outlook.com>
2019-06-26 11:54:20 +0100

    Project setup for client examples

---
+++ examples/SocketTplExtensions.ClientExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SocketTplExtensions.ClientExample
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Application started...");
+
+            try
+            {
+                await StartClient();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Fatal exception has been thrown");
+                Console.WriteLine(e);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Press <enter> to exit...");
+            Console.ReadLine();
+        }
+
+        private static async Task StartClient()
+        {
+
+        }
+    }
+}
