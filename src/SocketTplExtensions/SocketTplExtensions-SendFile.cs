861ed62d3fbf374503e1cab2f45a235d748983ba
João Simões <nogravity00@outlook.com>
2019-06-27 17:23:41 +0100

    Split SendToAsync into partial class

--- src/SocketTplExtensions/SocketTplExtensions-SendFile.cs
+++ src/SocketTplExtensions/SocketTplExtensions-SendFile.cs
@@ -3,9 +3,6 @@
 // ReSharper disable once CheckNamespace
 namespace System.Net.Sockets
 {
-    /// <summary>
-    /// Extensions for <see cref="Socket"/>
-    /// </summary>
     public static partial class SocketTplExtensions
     {
         /// <summary>
--- src/SocketTplExtensions/SocketTplExtensions.cs
+++ src/SocketTplExtensions/SocketTplExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-
-// ReSharper disable once CheckNamespace
+﻿// ReSharper disable once CheckNamespace
 namespace System.Net.Sockets
 {
     /// <summary>
@@ -8,38 +6,6 @@ namespace System.Net.Sockets
     /// </summary>
     public static partial class SocketTplExtensions
     {
-        #region SendToAsync
 
-        /// <summary>
-        /// Sends data asynchronously to a specific remote host.
-        /// </summary>
-        /// <param name="socket"></param>
-        /// <param name="buffer"></param>
-        /// <param name="offset"></param>
-        /// <param name="size"></param>
-        /// <param name="socketFlags"></param>
-        /// <param name="endpoint"></param>
-        /// <returns></returns>
-        public static Task<int> SendToAsync(this Socket socket, byte[] buffer, int offset, int size, SocketFlags socketFlags, EndPoint endpoint)
-        {
-            var tcs = new TaskCompletionSource<int>(socket);
-            socket.BeginSendTo(buffer, offset, size, socketFlags, endpoint, BeginSendToCallback, tcs);
-            return tcs.Task;
-        }
-
-        private static readonly AsyncCallback BeginSendToCallback = ar =>
-        {
-            var tcs = (TaskCompletionSource<int>) ar.AsyncState;
-            try
-            {
-                tcs.TrySetResult(((Socket) tcs.Task.AsyncState).EndSendTo(ar));
-            }
-            catch (Exception e)
-            {
-                tcs.TrySetException(e);
-            }
-        };
-
-        #endregion
     }
 }
