cf5bde9eb87d28d1962eee9133fec7f54757d618
João Simões <nogravity00@outlook.com>
2019-06-27 17:15:20 +0100

    Split AcceptAsync into parcial class

---
+++ src/SocketTplExtensions/SocketTplExtensions-Accept.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace System.Net.Sockets
+{
+    public static partial class SocketTplExtensions
+    {
+        /// <summary>
+        /// Begins an asynchronous operation to accept an incoming connection attempt.
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <returns></returns>
+        public static Task<Socket> AcceptAsync(this Socket socket)
+        {
+            var tcs = new TaskCompletionSource<Socket>(socket);
+            socket.BeginAccept(BeginAcceptCallback, tcs);
+            return tcs.Task;
+        }
+
+        private static readonly AsyncCallback BeginAcceptCallback = ar =>
+        {
+            var tcs = (TaskCompletionSource<Socket>) ar.AsyncState;
+            try
+            {
+                tcs.TrySetResult(((Socket) tcs.Task.AsyncState).EndAccept(ar));
+            }
+            catch (Exception e)
+            {
+                tcs.TrySetException(e);
+            }
+        };
+    }
+}
--- src/SocketTplExtensions/SocketTplExtensions.cs
+++ src/SocketTplExtensions/SocketTplExtensions.cs
@@ -9,35 +9,6 @@ namespace System.Net.Sockets
     /// </summary>
     public static partial class SocketTplExtensions
     {
-        #region AcceptAsync
-
-        /// <summary>
-        /// Begins an asynchronous operation to accept an incoming connection attempt.
-        /// </summary>
-        /// <param name="socket"></param>
-        /// <returns></returns>
-        public static Task<Socket> AcceptAsync(this Socket socket)
-        {
-            var tcs = new TaskCompletionSource<Socket>(socket);
-            socket.BeginAccept(BeginAcceptCallback, tcs);
-            return tcs.Task;
-        }
-
-        private static readonly AsyncCallback BeginAcceptCallback = ar =>
-        {
-            var tcs = (TaskCompletionSource<Socket>) ar.AsyncState;
-            try
-            {
-                tcs.TrySetResult(((Socket) tcs.Task.AsyncState).EndAccept(ar));
-            }
-            catch (Exception e)
-            {
-                tcs.TrySetException(e);
-            }
-        };
-
-        #endregion
-
         #region ReceiveAsync
 
         /// <summary>
