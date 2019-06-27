7977856529ee6e3d92332758c47e6b1235445e49
João Simões <nogravity00@outlook.com>
2019-06-27 17:13:47 +0100

    Split DisconnectAsync into partial class

---
+++ src/SocketTplExtensions/SocketTplExtensions-Disconnect.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace System.Net.Sockets
+{
+    public static partial class SocketTplExtensions
+    {
+        /// <summary>
+        /// Begins an asynchronous request to disconnect from a remote endpoint.
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="reuseSocket"></param>
+        /// <returns></returns>
+        public static Task DisconnectAsync(this Socket socket, bool reuseSocket)
+        {
+            var tcs = new TaskCompletionSource<bool>(socket);
+            socket.BeginDisconnect(reuseSocket, BeginDisconnectCallback, tcs);
+            return tcs.Task;
+        }
+
+        private static readonly AsyncCallback BeginDisconnectCallback = ar =>
+        {
+            var tcs = (TaskCompletionSource<bool>) ar.AsyncState;
+            try
+            {
+                ((Socket) tcs.Task.AsyncState).EndDisconnect(ar);
+                tcs.TrySetResult(true);
+            }
+            catch (Exception e)
+            {
+                tcs.TrySetException(e);
+            }
+        };
+    }
+}
--- src/SocketTplExtensions/SocketTplExtensions.cs
+++ src/SocketTplExtensions/SocketTplExtensions.cs
@@ -9,37 +9,6 @@ namespace System.Net.Sockets
     /// </summary>
     public static partial class SocketTplExtensions
     {
-        #region DisconnectAsync
-
-        /// <summary>
-        /// Begins an asynchronous request to disconnect from a remote endpoint.
-        /// </summary>
-        /// <param name="socket"></param>
-        /// <param name="reuseSocket"></param>
-        /// <returns></returns>
-        public static Task DisconnectAsync(this Socket socket, bool reuseSocket)
-        {
-            var tcs = new TaskCompletionSource<bool>(socket);
-            socket.BeginDisconnect(reuseSocket, BeginDisconnectCallback, tcs);
-            return tcs.Task;
-        }
-
-        private static readonly AsyncCallback BeginDisconnectCallback = ar =>
-        {
-            var tcs = (TaskCompletionSource<bool>) ar.AsyncState;
-            try
-            {
-                ((Socket) tcs.Task.AsyncState).EndDisconnect(ar);
-                tcs.TrySetResult(true);
-            }
-            catch (Exception e)
-            {
-                tcs.TrySetException(e);
-            }
-        };
-
-        #endregion
-
         #region AcceptAsync
 
         /// <summary>
