831f825fd4f43baf2d604a69dd4505011b1f065f
João Simões <nogravity00@outlook.com>
2019-06-27 17:12:28 +0100

    Split ConnectAsync into parcial class

--- src/SocketTplExtensions/SocketTplExtensions.cs
+++ src/SocketTplExtensions/SocketTplExtensions.cs
@@ -7,81 +7,8 @@ namespace System.Net.Sockets
     /// <summary>
     /// Extensions for <see cref="Socket"/>
     /// </summary>
-    public static class SocketTplExtensions
+    public static partial class SocketTplExtensions
     {
-        #region ConnectAsync
-
-        /// <summary>
-        /// Begins an asynchronous request for a remote host connection.
-        /// </summary>
-        /// <param name="socket"></param>
-        /// <param name="host"></param>
-        /// <param name="port"></param>
-        /// <returns></returns>
-        public static Task ConnectAsync(this Socket socket, string host, int port)
-        {
-            var tcs = new TaskCompletionSource<bool>(socket);
-            socket.BeginConnect(host, port, BeginConnectCallback, tcs);
-            return tcs.Task;
-        }
-
-        /// <summary>
-        /// Begins an asynchronous request for a remote host connection.
-        /// </summary>
-        /// <param name="socket"></param>
-        /// <param name="address"></param>
-        /// <param name="port"></param>
-        /// <returns></returns>
-        public static Task ConnectAsync(this Socket socket, IPAddress address, int port)
-        {
-            var tcs = new TaskCompletionSource<bool>(socket);
-            socket.BeginConnect(address, port, BeginConnectCallback, tcs);
-            return tcs.Task;
-        }
-
-        /// <summary>
-        /// Begins an asynchronous request for a remote host connection.
-        /// </summary>
-        /// <param name="socket"></param>
-        /// <param name="addresses"></param>
-        /// <param name="port"></param>
-        /// <returns></returns>
-        public static Task ConnectAsync(this Socket socket, IPAddress[] addresses, int port)
-        {
-            var tcs = new TaskCompletionSource<bool>(socket);
-            socket.BeginConnect(addresses, port, BeginConnectCallback, tcs);
-            return tcs.Task;
-        }
-
-        /// <summary>
-        /// Begins an asynchronous request for a remote host connection.
-        /// </summary>
-        /// <param name="socket"></param>
-        /// <param name="endpoint"></param>
-        /// <returns></returns>
-        public static Task ConnectAsync(this Socket socket, EndPoint endpoint)
-        {
-            var tcs = new TaskCompletionSource<bool>(socket);
-            socket.BeginConnect(endpoint, BeginConnectCallback, tcs);
-            return tcs.Task;
-        }
-
-        private static readonly AsyncCallback BeginConnectCallback = ar =>
-        {
-            var tcs = (TaskCompletionSource<bool>) ar.AsyncState;
-            try
-            {
-                ((Socket) tcs.Task.AsyncState).EndConnect(ar);
-                tcs.TrySetResult(true);
-            }
-            catch (Exception e)
-            {
-                tcs.TrySetException(e);
-            }
-        };
-
-        #endregion
-
         #region DisconnectAsync
 
         /// <summary>
